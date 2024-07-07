249f81169cd3453985671ae51a9db67b49edd33b
Yurii Kartsev <tinazov1999yura@gmaol.com>
2024-07-07 17:30:29 +0200

    Task_2 complet

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,29 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Task_2
+namespace Practika_5_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите предложения");
-
-
+            Console.WriteLine("Введите предложение");
+            string inputDataUser = Console.ReadLine();
+            string[] inputData = SplitText(inputDataUser);
+            ReverseText(inputData);
+            PrintWord(inputData);
         }
 
-        static string[] SplitText(string text) 
+        static string[] SplitText(string text)
         {
             string[] word = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (string s in word) 
-            {
-                Console.WriteLine(s);
-            }
             return word;
+        }
+
+        static void PrintWord(string[] text)
+        {
+            foreach (string n in text)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        static string[] ReverseText(string[] text)
+        {
+           Array.Reverse(text);
+            return text;
         }
     }
 }
